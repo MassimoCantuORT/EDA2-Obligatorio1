ed7e2db35df8f2028ce61264e08e5bc4512c3bc4
MassimoCantuORT <mc237773@fi365.ort.edu.uy>
2025-04-03 09:17:58 -0300

    updated test scripts

--- scriptt.sh
+++ scriptt.sh
@@ -8,19 +8,11 @@ if [ $# -ne 0 ]; then
     ej=$1
 fi
 
-find ./compilado/ -type f -name "*.exe" -exec rm {} \;
 find ./tests/_out/ -type f -name "*.out.txt" -exec rm {} \;
 find ./tests/_out/ -type f -name "*.diff.txt" -exec rm {} \;
+find ./tests/_out/ -type f -name "order.txt" -exec rm {} \;
 
-# Compile all .cpp files into executables
-mkdir -p compilado # Ensure the directory exists
-for item in *.cpp; do
-    if [[ ej -eq 0 ]] || [[ "$item" =~ ^ejercicio"$ej"\.cpp$ ]]; then
-        printf "Compilando %s\n" "$item"
-        g++ -o "./compilado/${item%.cpp}.exe" "$item" -std=c++11
-    fi
-done
-
+./scriptc.sh $ej
 
 mkdir -p ./tests/_out
 # Run each executable with each test input file
@@ -37,22 +29,27 @@ for item in ./compilado/*.exe; do
         continue
     fi
 
+    mkdir -p "./tests/_out/$base_item"
+
     for prueba in ./tests/"$base_item"/*.in.txt; do
-        startTime=$(date "+%s%3N")
+        
+        lines=$(wc -l < $prueba | bc)
         # Remove the directory path and .in.txt extension
         output_name="${prueba##*/}"          # Extract the base name of the file
         output_name="${output_name%.in.txt}" # Remove the .in.txt extension
-        mkdir -p "./tests/_out/$base_item"
-
-        ./"$item" <"$prueba" | dos2unix > "./tests/_out/$base_item/${output_name}.out.txt"
+        
+        startTime=$(date "+%s%3N")
+        ./"$item" <"$prueba" | unix2dos > "./tests/_out/$base_item/${output_name}.out.txt"
+        endTime=$(date "+%s%3N")
+        
         hash1=$(sha256sum "./tests/_out/$base_item/${output_name}.out.txt" | awk '{print $1}')
         hash2=$(sha256sum "./tests/$base_item/${output_name}.out.txt" | awk '{print $1}')
 
         # Compare the hashes
         if [ "$hash1" == "$hash2" ]; then
-            endTime=$(date "+%s%3N")
             totalTime=$(($endTime - $startTime))
             echo -e "${GREEN}Prueba $prueba OK${NC} Took ${totalTime}ms"
+            echo -e "$lines, $totalTime" >> "./tests/_out/$base_item/order.txt"
         else
             echo -e "${RED}ERROR: Prueba $prueba${NC}"
             diff "./tests/_out/$base_item/${output_name}.out.txt" "./tests/$base_item/${output_name}.out.txt" > "./tests/_out/$base_item/${output_name}.diff.txt"
